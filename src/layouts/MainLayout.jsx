f69d51054072ede2e0c3861eb1b434dd3e2d6646
Javascript Enthusiast <abmercy035@gmail.com>
2023-07-20 15:54:12 +0100

    minor :
    Created the layout comp

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 import TestComponent from '../components/TestComponent';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import NavBar from '../components/NavBar';
 
-export default function MainLayout({ children }) {
-console.log(children);
+export default function MainLayout(props) {
   return (
-    <div>
-      <TestComponent />
-      { children }
-      <Outlet />
+    <div className='l:w-800 w-full bg-red-800'>
+      { props.header && <Header /> }
+      <main>
+        <Outlet />
+      </main>
+      { props.nav && <NavBar /> }
+      { props.footer && <Footer /> }
     </div>
   )
 }
